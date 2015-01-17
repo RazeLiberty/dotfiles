9d2c16d94e1baed5196d40c28f8606901071029e
RazeLiberty <battledroid@outlook.jp>
2015-01-17 20:03:10 +0900

    _global系をインストーラーに追加した

--- dotfilesInstaller.sh
+++ dotfilesInstaller.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.gitignore ~/.gitignore
+ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/dotfiles/.gitattributes_global ~/.gitattributes_global
 ln -s ~/dotfiles/.vim/ ~/.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+ls -s ~/dotfiles/.module ~/.module
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
